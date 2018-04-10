590ee12fe1a6f40ddb6f7f470bd68c4b8ee03e4d
Eduardo Lago Aguilar <eduardo.lago.aguilar@gmail.com>
2018-04-10 09:52:13 -0300

    adding injected value to controller

--- springboot-demo1/src/main/java/springbootdemo/HelloController.java
+++ springboot-demo1/src/main/java/springbootdemo/HelloController.java
@@ -1,13 +1,18 @@
 package springbootdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping
-	public String index() {
-		return "Hello world!";
-	}
+    // inject via application.properties
+    @Value("${greetings.message:some default value}")
+    private String message;
+
+    @RequestMapping
+    public String index() {
+        return message;
+    }
 }
